42d7d1f4492a866789e3335b128cc44e1688624b
ken <1610057945@qq.com>
2023-04-18 11:25:36 +0800

    Initial commit

---
+++ build/CMakeFiles/RogueLike-Qt.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ken/Codes/RogueLike/RogueLike-Qt/build/RogueLike-Qt_autogen/mocs_compilation.cpp" "CMakeFiles/RogueLike-Qt.dir/RogueLike-Qt_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/RogueLike-Qt.dir/RogueLike-Qt_autogen/mocs_compilation.cpp.o.d"
+  "/Users/ken/Codes/RogueLike/RogueLike-Qt/main.cpp" "CMakeFiles/RogueLike-Qt.dir/main.cpp.o" "gcc" "CMakeFiles/RogueLike-Qt.dir/main.cpp.o.d"
+  "/Users/ken/Codes/RogueLike/RogueLike-Qt/mainwindow.cpp" "CMakeFiles/RogueLike-Qt.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/RogueLike-Qt.dir/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/RogueLike-Qt.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for RogueLike-Qt.
+# This may be replaced when dependencies are built.
